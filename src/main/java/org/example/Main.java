11b5000f8964aaccf077fce38c8493353bd4f74f
xBananaKing79x <khakimov.t@gmail.com>
2025-02-03 22:38:36 +0300

    Выполнение ДЗ-4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,21 @@ public class Main {
     public static void main(String[] args) {
 // Первая задача
         int personAge = 16;// количество лет
-        System.out.println(personAge >= 18 ? "Человек совершеннолетний" : "Человеку меньше 18, он не достиг совершеннолетия, нужно немного подождать"); //Тернарный оператор на определение возраста
+        if (personAge >= 18)//Условие совершеннолетия//
+        {
+            System.out.println("Человек совершеннолетний");
+        } else System.out.println("Человеку меньше 18, он не достиг совершеннолетия, нужно немного подождать");
 // Вторая задача
         byte outsideWeather = 7; // Указываем температуру на улице
-        if (outsideWeather <= 5) System.out.println("На улице - " + outsideWeather + " , нужно надеть шапку.");
-        else System.out.println("На улице - " + outsideWeather + ". Сегодня тепло, можно идти без шапки.");
+        if (outsideWeather <= 5) {
+            System.out.println("На улице - " + outsideWeather + " , нужно надеть шапку.");
+        } else System.out.println("На улице - " + outsideWeather + ". Сегодня тепло, можно идти без шапки.");
 // Третья задача
         short actualSpeed = 50;//Указываем текущую скорость
         short speedLimit = 60;//Указываем ограничение скорости
-        System.out.println(actualSpeed > speedLimit ? "Скорость превышена" : "Превышения скорости нет");
+        if (actualSpeed > speedLimit) {
+            System.out.println("Скорость превышена");
+        } else System.out.println("Превышения скорости нет");
 //Четвертая задача
         byte actualAge = 2;//Указываем текущий возраст
         if (actualAge > 2 && actualAge <= 6)//Возраст хождения в ДС
@@ -48,16 +54,19 @@ public class Main {
             System.out.println((Object) "В Вагоне есть сидячие места");
         } else if (trainPersonLimit > 60 && trainPersonLimit <= 102) {//Условие наличия стоячих мест
             System.out.println("В Вагоне остались только стоячие места");
-        } else if (trainPersonLimit>102) {//Условие отсутствия любых мест в вагоне (стоячих или сидячих)
+        } else if (trainPersonLimit > 102) {//Условие отсутствия любых мест в вагоне (стоячих или сидячих)
             System.out.println("В Вагоне нет мест");
         }
 //Седьмая задача
-        int one=2;
-        int two=3;
-        int three=2;
-        if (one>two && one>three) {System.out.println("Первое значение максимальное");}
-            else if (one<two && two>three) {System.out.println("Второе значение максимальное");}
-            else if (one<three && two<three) {System.out.println("Третье значение максимальное");}
-                else System.out.println("Максимального значения нет");
-        }
+        int one = 2;
+        int two = 3;
+        int three = 2;
+        if (one > two && one > three) {
+            System.out.println("Первое значение максимальное");
+        } else if (one < two && two > three) {
+            System.out.println("Второе значение максимальное");
+        } else if (one < three && two < three) {
+            System.out.println("Третье значение максимальное");
+        } else System.out.println("Максимального значения нет");
     }
+}
