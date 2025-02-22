efe1e949a38b75738fd5f60a28f2a39515af1eac
xBananaKing79x <khakimov.t@gmail.com>
2025-02-22 20:52:44 +0300

    Выполнение ДЗ-8 Массивы. Исправление по замечаниям 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,30 @@ public class Main {
         double[] arr4 = new double[]{1.57, 7.654, 9.986};
         int[] weights = {90, 91, 93, 92, 85, 87, 84, 83, 0, 0, 0, 0};
         System.out.println("Задача 2");
-        System.out.println(Arrays.toString(arr3));
-        System.out.println(Arrays.toString(arr4));
-        System.out.println(Arrays.toString(weights));
+        System.out.println("Первый массив:");
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i]);
+            if (i < arr3.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Второй массив:");
+        for (int k = 0; k < arr4.length; k++) {
+            System.out.print(arr4[k]);
+            if (k < arr4.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Третий массив:");
+        for (int j = 0; j < weights.length; j++) {
+            System.out.print(weights[j]);
+            if (j < weights.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
         System.out.println("Задача 3");
         for (int i = arr3.length - 1; i >= 0; i--) {
             System.out.print(arr3[i]);
@@ -43,6 +64,10 @@ public class Main {
         System.out.print(Arrays.toString(arr3));
     }
 }
+
+
+
+
 
 
 
