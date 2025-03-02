c7f901a28d41c86223b8fc5f5ff665cce0586778
xBananaKing79x <khakimov.t@gmail.com>
2025-03-02 17:24:13 +0300

    Выполнение ДЗ-11-Methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
 public class Main {
+    public static void printSeparator() {
+        System.out.println("++++++++++++++++");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Привет Мир!");
-        System.out.println("ДЗ Готово!");
+        System.out.println("Упражнение урока:");
+        int[] issuesByMonths = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
+        printSeparator();
+        for (int i = 0; i < issuesByMonths.length; i++) {
+            System.out.println(issuesByMonths[i]);
+            if ((i + 1) % 3 == 0) {
+                printSeparator();
+            }
+        }
+        printSeparator();
     }
 }
