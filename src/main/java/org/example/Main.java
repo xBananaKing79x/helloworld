c24fc568e4c4cb29c681960af9df08d1eb8dec1c
xBananaKing79x <khakimov.t@gmail.com>
2025-03-09 16:42:23 +0300

    Выполнение ДЗ-12-Objects&Classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир!");
-        System.out.println("ДЗ Готово!");
+        PersonService personService = new PersonService();
+        Person sarah = new Person("Sarah", 30);
+        if (personService.isAdult(sarah)) {
+            System.out.println("Иди в бар");
+        }
+        else {
+            System.out.println("Сиди дома");
+        }
+        System.out.println(sarah);
     }
 }
