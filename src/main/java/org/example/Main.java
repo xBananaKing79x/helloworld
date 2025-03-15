8bc33f5c8cd5d1eae2a5009af3543d7018728d98
xBananaKing79x <khakimov.t@gmail.com>
2025-03-15 20:13:00 +0300

    Выполнение coursework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,4 +10,59 @@ public class Main {
         employees[4] = new Employee ("Егор Воронкин", 3, 33000);
         employees[5] = new Employee ("Ирина Ястребова", 3, 46000);
     }
+    //Базовые методы
+    //Печать количества сотрудников
+    public static void printAllEmployees(){
+        for (Employee emp: employees){
+            if (emp !=null) System.out.println(emp);
+        }
+    }
+    //Печать фонда оплаты труда
+    public static double calculateTotalSalary(){
+        double total = 0;
+        for (Employee emp: employees) {
+            if (emp!=null) total +=emp.getSalary();
+        }
+        return total;
+    }
+    public static Employee findMinSalaryEmployee(){
+        Employee minEmp = null;
+        double min = Double.MAX_VALUE;
+        for (Employee emp: employees) {
+            if (emp!=null && emp.getSalary()<min) {
+                min = emp.getSalary();
+                minEmp=emp;
+            }
+        }
+        return minEmp;
+    }
+    public static Employee findMaxSalaryEmployee() {
+        Employee maxEmp = null;
+        double max = Double.MIN_VALUE;
+        for (Employee emp : employees) {
+            if (emp != null && emp.getSalary() > max) {
+                max = emp.getSalary();
+                maxEmp = emp;
+            }
+        }
+        return maxEmp;
+    }
+    public static double calculateAverageSalary() {
+        int count = 0;
+        double total = 0;
+        for (Employee emp : employees) {
+            if (emp != null) {
+                total += emp.getSalary();
+                count++;
+            }
+        }
+        return count > 0 ? total / count : 0;
+    }
+    public static void printFullNames() {
+        for (Employee emp : employees) {
+            if (emp != null) System.out.println(emp.getFullName());
+        }
+    }
+    //Методы повышенной сложности
+
 }
